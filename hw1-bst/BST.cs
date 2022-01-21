1a1f6671d5a77fd46cc04a6b6e0409972f9f610c
khurd21 <kyle.hurd@wsu.edu>
2022-01-21 14:36:09 -0800

    Designing Framework for Project
    Initialized solution containing the project for hw1-bst. Creating the solution was required in order to obtain debugging and Intellisense functionality within VSCode.
    
    - Added a BST.cs and Main.cs
         BST is currently just a class with a constructor, whereas Main.cs creates the object to show implementation correctly accepts the input string.
    
    - Included hw1-instructions to root for better access to requirements for project.

---
+++ hw1-bst/BST.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace BST
+{
+    public class BST
+    {
+
+        private Node root;
+        uint numItems;
+
+        private class Node
+        {
+            public uint data;
+            public Node left;
+            public Node right;
+
+            public Node(uint data)
+            {
+                this.data = data;
+                left = null;
+                right = null;
+            }
+        }
+
+        public BST(string data)
+        {
+            this.numItems = 0;
+            string[] dataArray = data.Split(' ');
+            foreach (string s in dataArray)
+            {
+                // Insert(uint.Parse(s));
+            }
+        }
+
+    }
+}
---
+++ hw1-bst/Main.cs
@@ -0,0 +1,14 @@
+using BST;
+
+namespace BST
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            BST bst = new BST("42 68 0 32 12");
+            
+        }
+    }
+
+}
