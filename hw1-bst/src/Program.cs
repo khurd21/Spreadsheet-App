1b41c78672fbaf0ba4e01d2c1c4e832d53117ea1
khurd21 <kyle.hurd@wsu.edu>
2022-01-23 23:38:06 -0800

    Created console interface and updated README
    The src/Program.cs now has a simple console interface for which to
    insert new numbers into the binary tree.
    
    Updated the README.md to include my name and student id.

--- hw1-bst/src/Program.cs
+++ hw1-bst/src/Program.cs
@@ -2,9 +2,43 @@ namespace BST
 {
     public class Program
     {
+
+        private static void displayBSTStats(BinaryTree bst)
+        {
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine($"Number of items: {bst.numItems}");
+            Console.WriteLine($"Height: {bst.getNumLevels()}");
+            Console.WriteLine($"Theoretical min height: {bst.theoreticalHeight()}");
+            Console.WriteLine("Inorder traversal:");
+            bst.inorderTraversal();
+            Console.WriteLine("**********************************************************");
+        }
+
+        private static bool makeInsert(BinaryTree bst, string input)
+        {
+            if (input == "q")
+            {
+                return false;
+            }
+            else
+            {
+                uint insert = uint.Parse(input);
+                bst.Insert(insert);
+                return true;
+            }
+        }
+
         private static void Main()
         {
-            BinaryTree bst = new BinaryTree("42 68 0 32 12");
+            BinaryTree bst = new BinaryTree();
+
+            string input;
+            do
+            {
+                displayBSTStats(bst);
+                Console.Write("Enter a number to enter into the tree [q to quit]: ");
+                input = Console.ReadLine();
+            } while (makeInsert(bst, input));
         }
     }
 }
