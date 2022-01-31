b8a01d61812c5324d2a7d84125e9ef4a359f673c
khurd21 <kyle.hurd@wsu.edu>
2022-01-30 22:57:00 -0800

    Initialized App
    Initialized the project as per homework requirements.
    The project requirements were not added to this commit but will be
    added in the future.
    
    Items Included in Solution:
    - A solution file linking the Unique and Unique.Test projects
    - All projects initialized with StyleCop and StyleCop.Analyzers
    - Generic template for Winforms in Unique
    - Generic template for NUnit in Unique.Test
    - .gitignore added, writting for VS application, vscode ignores are added as needed.

---
+++ hw2-unique/Unique.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Unique.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ hw2-unique/Unique/Form1.cs
@@ -0,0 +1,9 @@
+namespace Unique;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
