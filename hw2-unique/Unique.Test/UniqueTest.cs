f443ea21eb54ed9e9b08850241278c9e879b0325
khurd21 <kyle.hurd@wsu.edu>
2022-01-31 04:52:34 -0800

    Templating the UniqueClass Testcases
    There was an issue with the NUnit Testing in Unique.Test regarding the TargetFramework.
    The Unique target was net6.0-windows whereas the Unique.Test target was net6.0. This
    caused an error to throw when trying to build the test project. Solution was to restore
    project with target for net6.0-windows.
    
    First test case written for Unique Class Constructor. The constructor will initialize a
    List<uint> type with random numbers based on input parameters `size` and `max`.

--- hw2-unique/Unique.Test/UniqueTest.cs
+++ hw2-unique/Unique.Test/UniqueTest.cs
@@ -6,6 +6,8 @@
 namespace Unique.Test;
 
 using NUnit.Framework;
+using System.Threading.Tasks;
+using Unique;
 
 public class Tests
 {
@@ -14,9 +16,26 @@ public class Tests
     {
     }
 
+    /// <summary>
+    /// Tests the constructor of Unique class. The constructor should
+    /// initialize the array with the given size and max value.
+    /// This test ensures the array is initialized with the correct
+    /// size and max value.
+    /// </summary>
+    /// <param name="size">Capacity of the List.</param>
+    /// <param name="max">Maximum size of the number to be generated.</param>
     [Test]
-    public void Test1()
+    [TestCase(1, 1)]
+    [TestCase(100, 2)]
+    [TestCase(1000, 0)]
+    public void TestUniqueConstructor(uint size, uint max)
     {
-        Assert.Pass();
+        UniqueStatistics stats = new UniqueStatistics(size: size, max: max);
+        Assert.AreEqual(stats.Array.Count, size);
+        Parallel.ForEach<uint>(stats.Array, val =>
+        {
+            Assert.IsTrue(val <= max);
+            Assert.IsTrue(val >= 0);
+        });
     }
 }
