cffa1ae4ef88cae7e784ac86bca488ab471dfeab
khurd21 <kyle.hurd@wsu.edu>
2022-02-10 21:06:30 -0800

    TDD: FibonacciTextReaderTests
    Wrote the test cases for the public methods of FibonacciTextReader class.
    The FibonacciTextReader class is only partially implemented. The skeleton is provided,
    but additional work is needed for test cases to pass.

---
+++ Notepad/Notepad.Test/FibonacciTextReaderTests.cs
@@ -0,0 +1,119 @@
+// <copyright file="FibonacciTextReaderTests.cs" company="Washington State University">
+// Copyright (C) Kyle Hurd - Washington State University
+// Cpts 321 Spring 2022
+// </copyright>
+
+namespace Notepad.Test;
+
+using System;
+using NUnit.Framework;
+using Notepad.TextReader;
+using System.Numerics;
+
+/// <summary>
+/// Initializes the <see cref="FibonacciTextReaderTests"/> class.
+/// </summary>
+public class FibonacciTextReaderTests
+{
+    /// <summary>
+    /// Tests to ensure the correct fibonacci number is returned.
+    /// There is no limitation for capcity when using this function.
+    /// Capacity is only used when writing to a Stream.
+    /// </summary>
+    /// <param name="n">The nth Fibonacci.</param>
+    /// <param name="expectedStr">
+    /// The expected result of nth Fibonacci sequence. The expected value
+    /// is passed as a string to compensate for the BigInteger.
+    /// </param>
+    [Test]
+    [TestCase(-100, "-1")]
+    [TestCase(-1, "-1")]
+    [TestCase(0, "0")]
+    [TestCase(1, "1")]
+    [TestCase(2, "1")]
+    [TestCase(50, "12586269025")]
+    [TestCase(100, "354224848179261915075")]
+    public void TestFibonacciOf(in int n, string expectedStr)
+    {
+        BigInteger expected = BigInteger.Parse(expectedStr.ToString());
+        BigInteger actual = FibonacciTextReader.FibonacciOf(n);
+        Assert.AreEqual(expected, actual);
+    }
+
+    /// <summary>
+    /// Ensures the correct exception is thrown if an incorrect capacity is passed.
+    /// </summary>
+    /// <param name="capacity">The capacity to pass to FibonacciTextReader.</param>
+    [Test]
+    [TestCase(-100)]
+    [TestCase(-1)]
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(2)]
+    public void TestConstructorException(int capacity)
+    {
+        Assert.Throws<ArgumentException>(() => new FibonacciTextReader(capacity));
+    }
+
+    /// <summary>
+    /// Ensures the correct capacity is given to the <see cref="FibonacciTextReader"/> class.
+    /// </summary>
+    /// <param name="capacity">The capacity of fibonacci numbers it may output.</param>
+    [Test]
+    [TestCase(3)]
+    [TestCase(4)]
+    [TestCase(50)]
+    [TestCase(100)]
+    public void TestConstructorPass(int capacity)
+    {
+        FibonacciTextReader fibonacciTextReader = new FibonacciTextReader(capacity);
+        Assert.AreEqual(capacity, fibonacciTextReader.Capacity);
+    }
+
+    /// <summary>
+    /// This test exclusively tests the output string outputs the result of the
+    /// <see cref="FibonacciTextReader.FibonacciOf(int)"/> method along with the
+    /// nth fibonacci result it is attached to. In additions, it ensures it only has
+    /// the fibonacci numbers up to the capacity it was given.
+    /// </summary>
+    /// <param name="capacity">The total number of fibonacci values to output.</param>
+    [Test]
+    [TestCase(3)]
+    [TestCase(25)]
+    [TestCase(100)]
+    public void TestReadline(int capacity)
+    {
+        FibonacciTextReader fibonacciTextReader = new FibonacciTextReader(capacity);
+        for (int i = 0; i < capacity; ++i)
+        {
+            string content = fibonacciTextReader.ReadLine() ?? "ReadLine() returned null.";
+            BigInteger expected = FibonacciTextReader.FibonacciOf(i);
+            Assert.AreEqual($"{i}: {expected}\n", content);
+        }
+
+        Assert.IsNull(fibonacciTextReader.ReadLine());
+    }
+
+    /// <summary>
+    /// Ensures each line of the <see cref="FibonacciTextReader.ReadToEnd"/> method
+    /// lists each nth fibonacci along with its <see cref="FibonacciTextReader.FibonacciOf(int)"/>
+    /// result.
+    /// </summary>
+    /// <param name="capacity">The number of fibonacci numbers to calculate.</param>
+    [Test]
+    [TestCase(3)]
+    [TestCase(25)]
+    [TestCase(100)]
+    public void TestReadToEnd(int capacity)
+    {
+        FibonacciTextReader fibonacciTextReader = new FibonacciTextReader(100);
+        string content = fibonacciTextReader.ReadToEnd();
+        string[] lines = content.Split('\n');
+
+        for (int i = 0; i < capacity; ++i)
+        {
+            BigInteger expected = FibonacciTextReader.FibonacciOf(i);
+            Assert.AreEqual($"{i}: {expected}", lines[i]);
+        }
+    }
+}
---
+++ Notepad/Notepad/TextReader/FibonacciTextReader.cs
@@ -0,0 +1,97 @@
+// <copyright file="FibonacciTextReader.cs" company="Washington State University">
+// Copyright (C) Kyle Hurd - Washington State University
+// Cpts 321 Spring 2022
+// </copyright>
+
+namespace Notepad.TextReader;
+
+using System.IO;
+using System.Collections.Concurrent;
+using System.Numerics;
+
+/// <summary>
+/// Initializes the <see cref="FibonacciTextReader"/> class.
+/// </summary>
+public class FibonacciTextReader : TextReader
+{
+    /// <summary>
+    /// The storage for the fibonacci numbers for quick access.
+    /// </summary>
+    private static ConcurrentDictionary<int, BigInteger> fibonacciItems = new ConcurrentDictionary<int, BigInteger>();
+
+    /// <summary>
+    /// The current position in the fibonacci sequence.
+    /// </summary>
+    private int capacityLeft;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FibonacciTextReader"/> class.
+    /// </summary>
+    /// <param name="capacity">The maximum number of lines available.</param>
+    /// <exception cref="ArgumentException">Thrown when <see cref="Capacity"/> is less than three.</exception>
+    public FibonacciTextReader(int capacity)
+    {
+        if (capacity < 3)
+        {
+            throw new ArgumentException("Capacity must be greater than or equal to 3.");
+        }
+
+        this.Capacity = capacity;
+        this.capacityLeft = capacity;
+        if (fibonacciItems.Count < 3)
+        {
+            fibonacciItems.TryAdd(0, 0);
+            fibonacciItems.TryAdd(1, 1);
+            fibonacciItems.TryAdd(2, 1);
+        }
+    }
+
+    /// <summary>
+    /// Gets the limit of fibonacci numbers to be shown.
+    /// </summary>
+    public int Capacity
+    {
+        get;
+
+        private set;
+    }
+
+    /// <summary>
+    /// Calculates the Fibonacci number at <paramref name="n"/>.
+    /// </summary>
+    /// <param name="n">The index of which fibonacci to determine.</param>
+    /// <returns>The nth fibonacci number or -1 if <paramref name="n"/> is less than 0.</returns>
+    public static BigInteger FibonacciOf(int n)
+    {
+        if (n < 0)
+        {
+            return -1;
+        }
+
+        if (fibonacciItems.TryGetValue(3, out BigInteger result))
+        {
+            return result;
+        }
+
+        return 0;
+    }
+
+    /// <summary>
+    /// Returns the next fibonacci number up to the specified capacity.
+    /// </summary>
+    /// <returns>The next fibonacci number in sequence or null if reached capacity.</returns>
+    public override string? ReadLine()
+    {
+        this.capacityLeft--;
+        return null;
+    }
+
+/// <summary>
+/// Returns the fibonacci numbers up to the specified capacity.
+/// </summary>
+/// <returns>The string of fibonacci numbers up to its capacity.</returns>
+    public override string ReadToEnd()
+    {
+        return base.ReadToEnd();
+    }
+}
