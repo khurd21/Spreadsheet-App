d97d6f059bdd17ad6d8f51e06c0e234ede26ad90
khurd21 <kyle.hurd@wsu.edu>
2022-02-22 13:27:52 -0800

    Create Engine Class and relocated/renamed directories
    There are currently three projects within the SpreadSheet-Kyle-Hurd file.
    1. The Spreadsheet UI
    2. The SpreadsheetEngine
    3. The SpreadsheetEngine.Test
    
    The logic for the spreadsheet will be contained within the SpreadsheetEngine project as its
    respective Test project. I implemented the structure for the cell class which is ready to
    be written its logic.

---
+++ Spreadsheet-Kyle-Hurd/SpreadsheetEngine/Cell.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Cell.cs" company="Washington State University">
+// Copyright (C) Kyle Hurd - Washington State University
+// Cpts 321 Spring 2022
+// </copyright>
+
+namespace SpreadsheetEngine;
+
+/// <summary>
+/// Initializes the <see cref="Cell"/> class.
+/// </summary>
+public class Cell
+{
+}
