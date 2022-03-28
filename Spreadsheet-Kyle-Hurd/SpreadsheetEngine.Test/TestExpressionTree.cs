c92241f7ca08cbd103c2285befa1eeaa6e3a0b3f
khurd21 <kyle.hurd@wsu.edu>
2022-03-28 12:22:21 -0700

    TestGetVariables in wrong file
    The implemented test in this commit were implemented in the wrong file. It was relocated to the
    correct one.

--- Spreadsheet-Kyle-Hurd/SpreadsheetEngine.Test/TestExpressionTree.cs
+++ Spreadsheet-Kyle-Hurd/SpreadsheetEngine.Test/TestExpressionTree.cs
@@ -165,4 +165,23 @@ public class TestExpressionTree
         ExpressionTree tree = new ExpressionTree(expression);
         Assert.Throws<ArgumentException>(() => tree.Evaluate());
     }
+
+    /// <summary>
+    /// Tests the GetVariables method for returning a list of variables.
+    /// </summary>
+    /// <param name="expression">The expression under test.</param>
+    /// <param name="expected">The expexted variables to be returned from the method call.</param>
+    [Test]
+    [TestCase("A1", new string[] { "a1", })]
+    [TestCase("A1 + B4", new string[] { "a1", "b4" })]
+    [TestCase("C7 * B9 + 3", new string[] { "c7", "b9" })]
+    [TestCase("1+2", new string[] { })]
+    public void TestGetVariables(string expression, string[] expected)
+    {
+        ExpressionTree tree = new ExpressionTree(expression);
+        var actual = tree.GetVariables();
+        Console.WriteLine($"Expected: {string.Join(", ", expected)}");
+        Console.WriteLine($"Actual: {string.Join(", ", actual)}");
+        Assert.AreEqual(expected, actual);
+    }
 }
